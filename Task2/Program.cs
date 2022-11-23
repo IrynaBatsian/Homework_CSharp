318addfa04688e4ad93d8fe928105ebcf06c743e
Iryna <batsianiryna@gmail.com>
2022-11-23 09:39:16 +0300

    add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{ Console.Write("Большее число ");
+  Console.WriteLine(a);
+  Console.Write("Меньшее число ");
+  Console.WriteLine(b);
+}
+else if (b > a)
+{ Console.Write("Большее число ");
+  Console.WriteLine(b);
+  Console.Write("Меньшее число ");
+  Console.WriteLine(a);
+}
+else if (a == b)
+  Console.Write("Эти числа равны!");
