3eaf6cae35e2019681eb91fc93a301a9fec723a7
Iryna <batsianiryna@gmail.com>
2022-11-23 12:35:37 +0300

    add in Task 8 FOR

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,11 +1,5 @@
 ﻿Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int i = 1;
-
-while (i <= n)
-{
-  if (i % 2 == 0)
-  Console.Write($"{i} ");
-  i++;
-}
+for (int i = 2; i <= n; i += 2)
+Console.Write($"{i} ");
