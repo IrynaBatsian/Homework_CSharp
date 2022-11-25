6f8b1ce4f6cfa4f13cdd9c2ff55b56b79f8018a7
Iryna <batsianiryna@gmail.com>
2022-11-25 08:47:21 +0300

    add Another solution to the task4

---
+++ Task4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Another solution to the task
+Console.Clear();
+
+int i = 0; // entering a variable
+int maxNumbers = -2147000000; // entering the min value
+while (i < 3) // how many numbers should the user enter
+{
+    Console.Write("Введите число: "); 
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (maxNumbers < n) // checking the entered data
+        maxNumbers = n;
+
+    i++;
+}
+Console.Write(maxNumbers);
